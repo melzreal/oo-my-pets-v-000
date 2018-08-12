1c5bc132839a87095ce630e54f310a0a0b803a82
Melissa Real <melissa.real@hotmail.com>
2018-08-12 12:57:00 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,11 +1,13 @@
 class Fish
   attr_accessor :mood
   attr_reader :name
+  @@all = []
 
 
   def initialize(name, mood)
       @name = name
       @mood = nervous
+      @@all << self
   end
 
 end
